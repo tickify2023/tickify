6606920eb649b416e4b45f83a2f3f9d2e991dece
tickify2023 <134895920+tickify2023@users.noreply.github.com>
2024-05-11 18:50:28 -0300

    Update MainBanner.tsx

--- components/home/MainBanner.tsx
+++ components/home/MainBanner.tsx
@@ -6,11 +6,11 @@ export const MainBanner = () => {
       <div className="mainBanner">
         <div>
           <h1>Eventos</h1>
-          <h1 className="yellow">Exclusivos</h1>
+          <h1 className="yellow">Under</h1>
           {/* <p>La Meseta</p> */}
 
           <a href="#sec-events">
-            <button className="btn-action">Ver eventos</button>
+            <button className="btn-action">Construction</button>
           </a>
         </div>
       </div>
